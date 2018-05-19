358d47e59315cbad5da2104ae3e3ed691c59526f
karly.mareka@gmail.com <karly.mareka@gmail.com>
2018-05-19 19:05:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // write your code below!
 function happyHolidays() {
-  return "Happy Holidays!";
+  return "Happy Holidays!"
 }
 
 function happyHolidaysTo(name) {
-  return `Happy Holidays, ${name}!`;
+  return `Happy Holidays, ${name}!`
 }
 
 function happyHolidaysTo(holiday, name) {
-  return `Happy ${holiday}, ${name}!`;
+  return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown(holiday, days) {
-  return `It's ${days} days until ${holiday}!`;
+  return `It's ${days} days until ${holiday}!`
 }
